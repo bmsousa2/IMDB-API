bda470b858c4339b6cdf7c5461d627e86205a2b6
bmsousa2 <bruno.sousa.marcos@gmail.com>
2022-12-21 21:10:31 -0300

    - Carregando pacotes - Get Top 10 melhores filmes

---
+++ plumber_api.R
@@ -0,0 +1,33 @@
+# Carregando pacotes
+
+library('RMySQL')
+library('dplyr')
+library('plumber')
+
+############################
+# Construindo API
+###########################
+
+
+#* @apiTitle IMDB- API
+#* @apiDescription API para acessar base de dados do IMDB
+#* @apiVersion 1.0.0
+
+#* @apiTag Interacting Interagindo com os dados
+
+#* melhores filmes de todos os tempos
+#* @tag Interacting
+#* @get /TOP 10
+
+function() {
+  con = dbConnect(RMySQL::MySQL(),dbname = 'filmes', user = 'root', password = 'bruno2020', host = 'localhost')
+  
+  df_top_10 <- con %>%
+    tbl("filmes_imdb_res") %>%
+    select(movie_title,director_name, title_year, genres, imdb_score) %>%
+    arrange(-imdb_score) %>% 
+    collect()
+  
+  
+  return(head(df_top_10, n= 10))
+}
