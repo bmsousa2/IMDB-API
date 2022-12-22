c25a86db5d282f2ea4b27c3ff37aa50bd6107a78
bmsousa2 <bruno.sousa.marcos@gmail.com>
2022-12-21 21:21:11 -0300

    Get top5 melhores filmes por ano.

--- plumber_api.R
+++ plumber_api.R
@@ -31,3 +31,24 @@ function() {
   
   return(head(df_top_10, n= 10))
 }
+
+#* melhores filmes por ano
+#* @tag Interacting
+#* @param p_filter Nome da Coluna
+#* @param r_filter Ano do filme
+#* @get /TOP5 
+
+function(p_filter, r_filter) {
+  
+  con = dbConnect(RMySQL::MySQL(),dbname = 'filmes', user = 'root', password = 'bruno2020', host = 'localhost')
+  
+  df_best_film_year <- con %>%
+    tbl("filmes_imdb_res") %>%
+    select(movie_title,director_name, title_year, genres, imdb_score) %>%
+    filter(!!sym(p_filter) == r_filter) %>%
+    arrange(-imdb_score) %>% 
+    collect()
+  
+  head(df_best_film_year, n = 5)
+  
+}
